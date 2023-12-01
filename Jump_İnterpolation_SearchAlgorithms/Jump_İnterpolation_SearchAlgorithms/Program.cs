b8f0bb0a7a6121653ded4ea7733a6a5b262540de
seferovrusif <seferovrusif@gmail.com>
2023-12-01 23:12:19 +0400

    ad

---
+++ Jump_İnterpolation_SearchAlgorithms/Jump_İnterpolation_SearchAlgorithms/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Jump_İnterpolation_SearchAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //inputs for Interpolation Search Algorithm 
+            int[] arr = { 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 16, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19, 19, 20, 20, 21 };
+            int a = 12;  //the element we are looking for
+
+            Console.WriteLine("InterpolationSearchAlgorithm's Result: ");
+            SearchAlgorithms.InterpolationSearchAlgorithm(arr, a);
+
+
+
+            //inputs for Jump Search Algorithm
+            int[] arrr = { 0, 0, 2, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 };
+            int b = 33;   //the element we are looking for
+
+            Console.WriteLine("\nJumpSearchAlgorithm's Result: ");
+            SearchAlgorithms.JumpSearchAlgorithm(arrr, b);
+
+        }
+    }
+}
---
+++ Jump_İnterpolation_SearchAlgorithms/Jump_İnterpolation_SearchAlgorithms/SearchAlgorithms.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jump_İnterpolation_SearchAlgorithms
+{
+        internal static class SearchAlgorithms
+        {
+            public static void InterpolationSearchAlgorithm(int[] arr, int x)
+            {
+                //Interpolation Search
+                //It is mainly effective when there are many consecutive and repeated numbers
+
+                int n = arr.Length;
+                int index = -1;
+                int pos = 0;
+                int lo = 0;
+                int hi = n - 1;
+                for (; true;)
+                {
+                    if (lo <= hi && x >= arr[lo] && x <= arr[hi])
+                    {
+
+                        pos = lo + (((hi - lo) / (arr[hi] - arr[lo])) * (x - arr[lo]));
+                        if (arr[pos] == x)
+                        {
+                            index = pos;
+
+                            break;
+                        }
+                        if (arr[pos] < x)
+                        {
+                            lo = pos + 1;
+
+                            continue;
+                        }
+                        if (arr[pos] > x)
+                        {
+                            hi = pos - 1;
+
+                            continue;
+                        }
+
+                    }
+                    else
+                    {
+                        index = -1;
+                        break;
+                    }
+                }
+                if (index != -1)
+                    Console.WriteLine("Element found at index " + index + "\n");
+                else
+                    Console.WriteLine("Element not found.\n");
+
+            }
+
+
+            public static void JumpSearchAlgorithm(int[] arr, int x)
+            {
+                int n = arr.Length;
+                int index = -1;
+                int step = (int)Math.Sqrt(n);
+                int prev = 0;
+                for (int minStep = Math.Min(step, n) - 1; arr[minStep] < x; minStep = Math.Min(step, n) - 1)
+                {
+                    prev = step;
+                    step += (int)Math.Sqrt(n);
+                    if (prev >= n)
+                    {
+                        index = -1;
+                        break;
+                    }
+                }
+                while (arr[prev] < x)
+                {
+                    prev++;
+                    if (prev == Math.Min(step, n))
+                    {
+                        index = -1;
+                        break;
+                    }
+                }
+                if (arr[prev] == x)
+                {
+                    index = prev;
+                }
+
+                if (index == -1)
+                {
+                    Console.WriteLine("Element not found!\n");
+                }
+                else
+                {
+                    Console.Write("Number " + x + " is at index " + index+"\n");
+                }
+
+            }
+
+        }
+    }
